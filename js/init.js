342fb421880385cbebab9a110a9d0e3e46848e61
Reid <jreid.greer@gmail.com>
2016-06-07 07:39:13 -0700

    Further implement one page scroll

--- js/init.js
+++ js/init.js
@@ -1,8 +1,21 @@
-$(document).on('scroll', function() {
-  console.log('scrolling', $(document).scrollTop());
-  if($(document).scrollTop() > 25) {
-    $('#header').addClass('small-header');
-  } else {
-    $('#header').removeClass('small-header');
-  }
-})
+// $(document).on('scroll', function() {
+//   console.log('scrolling', $(document).scrollTop());
+//   if($(document).scrollTop() > 25) {
+//     $('#header').addClass('small-header');
+//   } else {
+//     $('#header').removeClass('small-header');
+//   }
+// })
+
+$('.main').onepage_scroll({
+  sectionContainer: 'section',
+  easing: 'ease',
+  pagination: false,
+  beforeMove: function(index){
+    $('.menu-active').removeClass('menu-active');
+    $('#menu_section_' + index).addClass('menu-active');
+  },
+  loop: false,
+  keyboard: true,
+  responsiveFallback: false,
+});
